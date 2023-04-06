7a93baa1e68cbd46212f2deeed20b6f320646a52
Enzo Averlant <enaverlant@gmail.com>
2023-04-06 17:43:16 +0200

    :bug: fix: fixing fonts on netlify build

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,15 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      $fonts: resolve('./public/assets/fonts')
     }
   }
 })
